887f855e99d633b5c36cd68ddf4851c58580a39d
Davi Oliveira <davi.ol.santos@gmail.com>
2025-08-11 10:08:32 -0300

    build: estruturando projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Educação Infantil Bilíngue | Colégio Sorocaba",
+  description: "Colégio Sorocaba: Educação Infantil bilíngue com acolhimento e ensino de inglês desde cedo, utilizando o programa Etapa Idiomas para formar cidadãos do mundo preparados para o futuro.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
